bc46f224e1ef9a4d4201ea4ada2bbcadd0362d5c
jaasim <it24103137@my.sliit.lk>
2026-04-26 09:42:51 +0530

    Initial clean commit without node_modules

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import itemRoutes from "./routes/itemRoutes.js";
+import dns from "dns";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Item Manager API is running..." });
+});
+
+app.use("/api/items", itemRoutes);
+
+const PORT = process.env.PORT || 5000;
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  });
